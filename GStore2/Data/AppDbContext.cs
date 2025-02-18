41628c5055c889ea4bbe7201fcb90067f2a2adef
yasminpreciato <yasminfpreciato2019@gmail.com>
2025-02-17 22:31:06 -0300

    Aula 17.02.2025-Fazendo Banco de dados

--- GStore2/Data/AppDbContext.cs
+++ GStore2/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
+using System.Configuration;
 using GStore2.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,4 +21,87 @@ namespace GStore2.Data;
       public DbSet<Usuario> Usuarios { get; set; }
 
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Usuario>().ToTable("usuario");
+        builder.Entity<IdentityRole>().ToTable("perfil");
+        builder.Entity<IdentityUserRole<string>>().ToTable("usuario_perfil");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("usuario_regra");
+        builder.Entity<IdentityUserToken<string>>().ToTable("usuario_token");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("usuario_Login");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("perfil_regra");
+
+       #region Popular Categoria
+
+       List<Categoria> categorias = new(){
+        new(){
+          Id = 1,
+          Nome = "Eletronicos"
+        },
+
+        new() {
+          Id = 2,
+          Nome = "Celulares"
+        }
+       };
+       builder.Entity<Categoria>().HasData(categorias);
+        #endregion
+
+      #region Popular Usuario
+        Usuario usuario = new(){
+          Id = Guid.NewGuid().ToString(),
+          UserName ="GalloJunior",
+          NormalizedUserName = "GALLOJUNIOR",
+          Email = "gallojunior@gmail.com",
+          NormalizedEmail = "GALLOJUNIOR@GMAIL.COM",
+          EmailConfirmed = true,
+          Nome = "jose Atonio Gallo Junior",
+          DataNascimento = DateTime.Parse("05/08/1981"),
+          LockoutEnabled = true
+        };
+        PasswordHasher<Usuario> password = new();
+        password.HashPassword(usuario, "123456");
+        builder.Entity<Usuario>().HasData(usuario);
+      #endregion
+
+      #region Popular Perfil
+        List<IdentityRole> perfis = new()
+        {
+          new()
+          {
+             Id = Guid.NewGuid().ToString(),
+             Name ="Administrador",
+             NormalizedName = "ADMINISTRADOR"
+
+          },
+          new()
+          {
+             Id = Guid.NewGuid().ToString(),
+             Name ="Funcionario",
+             NormalizedName = "FUNCIONARIO"
+
+          },
+          new()
+          {
+             Id = Guid.NewGuid().ToString(),
+             Name ="Cliente",
+             NormalizedName = "CLIENTE"
+          }
+        };
+        builder.Entity<IdentityRole>().HasData(perfis);
+      #endregion
+
+       #region Popular Usuario-Perfil
+       List<IdentityUserRole<string>> userRoles = new()
+       {
+          new()
+          {
+            UserId= usuario.Id,
+            RoleId = perfis[0].Id
+          }
+       };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+       #endregion
     }
+}
--- GStore2/Program.cs
+++ GStore2/Program.cs
@@ -1,9 +1,37 @@
+using Google.Protobuf.WellKnownTypes;
+using GStore2.Data;
+using GStore2.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Serviço da conexão com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("GStore2Conn");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+// Serviço de getão do Usuario - Identity
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    Options => Options.SignIn.RequireConfirmedEmail = false 
+  
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+
 var app = builder.Build();
+
+// Garantir que o banco exista ao o projeto
+using (var scope = app.Services.CreateScope())
+{
+    var DbContext = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();   
+    await DbContext.Database.EnsureCreatedAsync(); 
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
