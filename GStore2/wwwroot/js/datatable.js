d3d6326b3fd01c5e4d786207168c1f1ce95a562a
yasminpreciato <yasminfpreciato2019@gmail.com>
2025-03-14 22:38:13 -0300

    Estilizando o layout Administrativi e adicionando dataTable

---
+++ GStore2/wwwroot/js/datatable.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const table = document.getElementById("table");
+    if (table){
+        const dataTable = new DataTable(table);
+    }
+});
